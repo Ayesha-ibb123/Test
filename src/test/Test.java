88f3b899a5a7828538b1395f2b3e466397c77882
Ayesha <Ayesha@DESKTOP-EGAH60G>
2023-07-02 21:41:06 +0530

    static and non static method

---
+++ src/test/Test.java
@@ -0,0 +1,37 @@
+package test;
+
+public class Test {
+	
+	static void speak()
+	   {
+		   System.out.println("Hi ayesha");
+	   }
+		   
+		   public static void main(String[] args) {
+			  
+			   Test t1=new Test();
+	    	   t1.display();
+	    	   Test.speak();
+		       
+	   
+	   }
+
+       void display()
+       {
+    	   System.out.println("hey");
+       }
+       
+    	   
+    	   
+       }
+       
+
+
+	
+	
+   
+	
+	
+
+
+
