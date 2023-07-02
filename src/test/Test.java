b38706a556029b71b374495e3c71e636ef17189b
Ayesha <Ayesha@DESKTOP-EGAH60G>
2023-07-02 21:41:33 +0530

    push

--- src/test/Test.java
+++ src/test/Test.java
@@ -8,10 +8,10 @@ public class Test {
 	   }
 		   
 		   public static void main(String[] args) {
-			  
+			   Test.speak();
 			   Test t1=new Test();
 	    	   t1.display();
-	    	   Test.speak();
+	    	  
 		       
 	   
 	   }
