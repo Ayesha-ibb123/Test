237b72df6de84d0285eb44ac0b5fdee6c3d30c03
Ayesha <Ayesha@DESKTOP-EGAH60G>
2023-07-08 23:56:37 +0530

    push8

---
+++ src/test/Demo.java
@@ -0,0 +1,31 @@
+package test;
+
+public class Demo {
+	public static void main(String[] args){
+
+	    A a1 = new A();
+
+	    a1.method1().method2();
+	    System.out.println(a1);
+
+	  }
+
+	}
+ class A{
+
+	  A method1(){
+		
+	    return this;
+
+	  }
+
+	  A method2(){
+
+	    return this;
+
+	  }
+
+	}
+
+
+
