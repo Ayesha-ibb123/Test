bd130157be50cfc42829fdb1fbca218ec3700e1a
Ayesha <Ayesha@DESKTOP-EGAH60G>
2023-07-20 01:05:59 +0530

    pul4

---
+++ src/test/Practice.java
@@ -0,0 +1,30 @@
+package test;
+
+public class Practice {
+	
+	static{
+
+		System.out.println("first block");  
+
+		  }
+
+		  static{
+
+		System.out.println("second block");  
+
+		  }
+
+
+
+		public static void main(String[] args){
+
+		System.out.println("main method");
+
+		}
+
+
+
+		  }
+
+
+
